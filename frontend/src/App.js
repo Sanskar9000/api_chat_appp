2ca1babbe0afbc778482da67b6d7fb7f5b8f1b37
Gunjan Solanki <gsolanki@isystango.com>
2021-01-13 22:04:37 +0530

    Setup routing & helper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,61 @@
 import './App.css';
+import React from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import Header from './components/Header';
+import { routes } from './routes';
+import { isAuthenticated } from "./utils";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const RenderRoute = (route) => {
+    document.title = route.title;
+    const history = useHistory({});
+    if(route.needAuth && !isAuthenticated()) {
+        history.push("/auth/login");
+    }
+
+    return (
+        <Route  
+            path={route.path} 
+            exact
+            render={(props) => <route.component {...props} />}
+        >
+        </Route>
+    );
+};
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentUser: null,
+            allRooms: [],
+            currentRoom: {
+                room: {}, 
+                users: [],
+                messages: []
+            }
+        }
+    }
+
+    handleLogout = () => {
+        return null
+    }
+
+    render() {
+        
+        return (
+            <div>
+                <Switch>
+                    <Header currentUser={this.state.currentUser} logout={this.handleLogout} />
+                    {routes.map((route, index) => {
+                        <RenderRoute key={index} {...route} />
+                    })}
+                </Switch>
+            </div>
+          
+        );
+    }
 }
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { APP_CABLE_URL } from './constants';
 import actionCable from 'actioncable';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const cable = actionCable.createConsumer(APP_CABLE_URL)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App cable={cable}/>
+    <Router>
+      <App cable={cable}/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
