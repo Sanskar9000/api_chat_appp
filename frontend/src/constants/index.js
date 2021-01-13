bb9271ff88c92fef40e7893f82704723117973a0
Gunjan Solanki <gsolanki@isystango.com>
2021-01-13 18:06:08 +0530

    Attach action cable

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,1 @@
+export const APP_CABLE_URL="ws://localhost:3000/cable";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { APP_CABLE_URL } from './constants';
+import actionCable from 'actioncable';
+
+const cable = actionCable.createConsumer(APP_CABLE_URL)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cable={cable}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
