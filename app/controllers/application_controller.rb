38cde3b9799fc68524b71ee6d6fad6d41be76beb
Gunjan Solanki <gsolanki@isystango.com>
2021-01-14 22:51:21 +0530

    Add jwt methods & encode token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+
 class ApplicationController < ActionController::API
+  
+  def encode(payload)
+    JsonWebToken.encode(payload)
+  end
+
+  def decode(token)
+    JsonWebToken.decode(token)
+  end
 end
